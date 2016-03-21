f723ef333931af38cfc27c8c2ca9cb7863b5b44f
Thorin Butler <thorin.butler@gmail.com>
2016-03-21 18:08:34 -0500

    created artists model via command line

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,10 @@
 FactoryGirl.define do
+  factory :artist do
+    name "MyString"
+    genre "MyString"
+    origin "MyString"
+    website "MyString"
+    bio "MyString"
+    image "MyString"
+  end
 end
