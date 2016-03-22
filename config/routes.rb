a4b35d16c9d38ccbddef04ae55b72b8b33f0f91d
Thorin Butler <thorin.butler@gmail.com>
2016-03-22 15:58:13 -0500

    installed administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+resources :articles
+resources :artists
+resources :events
+resources :plans
+resources :tickets
+resources :venues
+
+    root to: "users#index"
+  end
+
   devise_for :users
   root to: "misc#home"
 end
