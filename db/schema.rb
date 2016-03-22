dd74aa7721df387f6630eae3e4d45660f4b82418
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-22 11:14:44 -0500

    changed user_id in articles from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322151824) do
+ActiveRecord::Schema.define(version: 20160322152428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.string   "article"
     t.string   "image"
     t.datetime "created_at", null: false
