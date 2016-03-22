0ab9d66f75184a9da5588002abfe6012b37cad2f
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-22 10:11:48 -0500

    event age-range now event minimum_age

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320220315) do
+ActiveRecord::Schema.define(version: 20160322144618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "byline"
+    t.string   "article"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.string   "origin"
+    t.string   "website"
+    t.string   "bio"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -31,5 +51,50 @@ ActiveRecord::Schema.define(version: 20160320220315) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "artist_id"
+    t.integer  "venue_id"
+    t.string   "minimum_age"
+    t.datetime "start_date_time"
+    t.datetime "end_date_time"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.integer  "phone_number"
+    t.integer  "event_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string   "name"
+    t.string   "link"
+    t.integer  "price_in_cents"
+    t.integer  "eventbrite_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "blurb"
+    t.string   "profile_pic"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "info"
+    t.string   "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
