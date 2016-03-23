b3aaa1a570c089ce0f777ff55d70bb1cb072906f
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-23 14:12:37 -0500

    added tomdocs

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,7 @@
 class Venue < ActiveRecord::Base
+  ## allevents find all events at given venue
+  #
+  # Returns Array of Event objects
   def allevents
     Event.where("venue_id" => self.id)
   end
