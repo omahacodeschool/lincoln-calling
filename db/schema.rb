67c3dbaf7af3972fb84ec09c86d77fb3360c0318
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-24 13:27:43 -0500

    almost forgot info as text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324181755) do
+ActiveRecord::Schema.define(version: 20160324182457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 20160324181755) do
   create_table "venues", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
-    t.string   "info"
+    t.text     "info"
     t.string   "img"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
