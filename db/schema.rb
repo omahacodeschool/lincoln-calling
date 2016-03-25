e9b4e9b59c760d2b99991fc471d32047c9d436d5
Thorin Butler <thorin.butler@gmail.com>
2016-03-25 11:16:48 -0500

    had to add in two columns to the user schema created_at and updated_at...working now

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324214737) do
+ActiveRecord::Schema.define(version: 20160325161052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 20160324214737) do
     t.string   "profile_pic"
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
