7a2b004378a865fab3e8f250f1e74f13baf9b206
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-25 15:24:06 -0500

    add type to artists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325173535) do
+ActiveRecord::Schema.define(version: 20160325202305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20160325173535) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "headline_order"
-    t.boolean  "is_band"
+    t.string   "type"
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
