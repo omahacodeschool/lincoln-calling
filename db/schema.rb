9ed3042a7b5b1ad2af1462cbeb768ebed67256fd
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-26 21:33:00 -0500

    removed the previous news_or_visitor column from articles since now that will be type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327001905) do
+ActiveRecord::Schema.define(version: 20160327023156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20160327001905) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.datetime "publish_date_time"
-    t.string   "news_or_visitor"
     t.string   "type"
   end
 
