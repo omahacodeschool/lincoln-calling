62930f5e4d88a18a0328c6f83728470fc9e91a7d
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-26 21:48:14 -0500

    dashboards for article classes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     resources :sponsors
     resources :bands
     resources :comedians
+    resources :visitors
+    resources :features
     root to: "users#index"
   end
 
