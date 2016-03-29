338170227cc9ca2457f7cf008ee2ec3594095000
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-29 15:32:48 -0500

    fixed band problem have to incorporate new divs from front end

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -4,6 +4,7 @@ class MiscController < ApplicationController
     articles = Feature.all.order("id DESC").limit(4)
     @featurearticle = articles[0]
     @articles = articles[1..3]
-    @artists= Band.order(:headline_order).limit(3)
+    @headlineartists= Band.order(:headline_order).limit(3)
+    @otherartists = Band.where('headline_order != ?', 1).where('headline_order != ?', 2).where('headline_order != ?', 3)
   end  
 end
