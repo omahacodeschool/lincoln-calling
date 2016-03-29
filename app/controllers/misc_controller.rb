d94ad7098838f3fbe9a43a4963ea4aea233f4cdd
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-29 10:46:37 -0500

    articles displaying on homepage

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,5 +1,7 @@
 class MiscController < ApplicationController
   def home
     @events = Event.all
+    #once we have full web views, this number will have to be bigger
+    @articles = Feature.all.order("id DESC").limit(1)
   end  
 end
