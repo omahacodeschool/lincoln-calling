3cda6007f6a0c9a4f268b672d260013858cc5985
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-30 14:52:01 -0500

    pull master for thorin's changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,44 +40,44 @@ venue_list.each do |name, address, info, img, coordinates_latitude, coordinates_
 end
 
 feature_list = [
-  ["Test Article 1: A Long Titled Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg",2016-05-26 21:55:19],
-  ["Test Article 2", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg",2016-08-26 21:55:19],
-  ["Test Article 3 A Very Fascinating Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg",2016-05-15 21:55:19]
+  ["Test Article 1: A Long Titled Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg","2016-05-26 21:55:19"],
+  ["Test Article 2", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg","2016-08-26 21:55:19"],
+  ["Test Article 3 A Very Fascinating Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg","2016-05-15 21:55:19"]
 ]
 feature_list.each do |title, user_id, article_content, image, publish_date_time|
   Feature.create(title: title, user_id: user_id, article_content: article_content, image: image, publish_date_time: publish_date_time)
 end
 
 faq_list = [
-  ["Test FAQ 1 Question Question", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content",2016-05-26 21:55:19],
-  ["Test FAQ 2 Question Question Question Question", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content",2016-08-26 21:55:19],
-  ["Test FAQ 3 Question Question", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content",2016-05-15 21:55:19]
+  ["Test FAQ 1 Question Question", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content","2016-05-26 21:55:19"],
+  ["Test FAQ 2 Question Question Question Question", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content","2016-08-26 21:55:19"],
+  ["Test FAQ 3 Question Question", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content","2016-05-15 21:55:19"]
 ]
 faq_list.each do |title, user_id, article_content, publish_date_time|
   Faq.create(title: title, user_id: user_id, article_content: article_content, publish_date_time: publish_date_time)
 end
 
 visitor_list = [
-  ["Test Article 1: A Long Titled Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg",2016-05-26 21:55:19],
-  ["Test Article 2", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg",2016-08-26 21:55:19],
-  ["Test Article 3 A Very Fascinating Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg",2016-05-15 21:55:19]
+  ["Test Article 1: A Long Titled Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg","2016-05-26 21:55:19"],
+  ["Test Article 2", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg","2016-08-26 21:55:19"],
+  ["Test Article 3 A Very Fascinating Article", 1, "content content content content content content content content content content content content content content content content content content content content content content content content content", "/assets/beutler.jpg","2016-05-15 21:55:19"]
 ]
 visitor_list.each do |title, user_id, article_content, image, publish_date_time|
   Visitor.create(title: title, user_id: user_id, article_content: article_content, image: image, publish_date_time: publish_date_time)
 end
 
-About.create(title: "About", user_id: 1, article_content: "content content content content content content content content content content content content content content content content content content content content content content content content content",2016-05-26 21:55:19)
+About.create(title: "About", user_id: 1, article_content: "content content content content content content content content content content content content content content content content content content content content content content content content content",publish_date_time: "2016-05-26 21:55:19")
 
 events_list = [
-  [1,1,21,2016-10-06 19:00:00,2016-10-06 19:30:00],
-  [2,2,18,2016-10-06 21:00:00,2016-10-06 21:30:00],
-  [3,3,0,2016-10-06 22:00:00,2016-10-06 23:30:00],
-  [4,1,21,2016-10-07 19:00:00,2016-10-07 21:30:00],
-  [5,2,18,2016-10-07 21:00:00,2016-10-07 21:30:00],
-  [6,1,21,2016-10-07 22:00:00,2016-10-07 22:30:00],
-  [7,3,0,2016-10-08 19:00:00,2016-10-08 19:30:00],
-  [8,2,18,2016-10-08 21:00:00,2016-10-08 22:30:00],
-  [9,1,21,2016-10-08 22:00:00,2016-10-08 22:30:00],
+  [1,1,21,"2016-10-06 19:00:00","2016-10-06 19:30:00"],
+  [2,2,18,"2016-10-06 21:00:00","2016-10-06 21:30:00"],
+  [3,3,0,"2016-10-06 22:00:00","2016-10-06 23:30:00"],
+  [4,1,21,"2016-10-07 19:00:00","2016-10-07 21:30:00"],
+  [5,2,18,"2016-10-07 21:00:00","2016-10-07 21:30:00"],
+  [6,1,21,"2016-10-07 22:00:00","2016-10-07 22:30:00"],
+  [7,3,0,"2016-10-08 19:00:00","2016-10-08 19:30:00"],
+  [8,2,18,"2016-10-08 21:00:00","2016-10-08 22:30:00"],
+  [9,1,21,"2016-10-08 22:00:00","2016-10-08 22:30:00"]
 ]
 events_list.each do |artist_id, venue_id, minimum_age, start_date_time, end_date_time|
   Event.create(artist_id: artist_id, venue_id: venue_id, minimum_age: minimum_age, start_date_time: start_date_time, end_date_time: end_date_time)  
@@ -91,8 +91,8 @@ sponsors_list = [
   ["Other Sponsor2", "google.com",false,"content content content content",4,"/assets/images/lincoln.png"],
   ["Other Sponsor3", "google.com",false,"content content content content",4,"/assets/images/lincoln.png"]
 ]
-sponsors_list.each do |name, website, premiere, description, sponsor_order, logo|
-  Sponsor.create(name: name, website: website, premiere: premiere, description:description, sponsor_order:sponsor_order, logo:logo)
+sponsors_list.each do |name, website, premier, description, sponsor_order, logo|
+  Sponsor.create(name: name, website: website, premier: premier, description:description, sponsor_order:sponsor_order, logo:logo)
 end
 
 tickets_list = [
