4991eb15d707bba36f36dda448b377c7e7b2a936
Thorin Butler <thorin.butler@gmail.com>
2016-03-30 12:21:30 -0500

    redundancies removed from models that we've given associations to and functionality seems to be a go

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,28 +3,6 @@
 class Artist < ActiveRecord::Base
   has_many(:events)
 
-  def get_venue_ids
-    venue_ids = []
-  def venue_name
-    venue_name = []
-    x = self.event_venue_id
-    y = Venue.where(:id => x)
-    y.each do |venue|
-      venue_name << venue.name
-    end
-    return venue_name
-  end
-
-  def venue_address
-    venue_address = []
-    x = self.event_venue_id
-    y = Venue.where(:id => x)
-    y.each do |venue|
-      venue_address << venue.address
-    end
-    return venue_address
-  end
-
   mount_uploader :image, ArtistImageUploader
 
 end
