d003ffc4c67d859f3b303fa0a2f53ae75b343655
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-30 12:08:53 -0500

    seeds of bands and comedians

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+band_list = [
+  [ "Future Islands", "Rock", "Maryland", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 1, "Band" ],
+  [ "Boy", "Rock", "Germany", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 2, "Band" ],
+  [ "The Kickback", "Pop", "Illinois", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 3, "Band" ],
+  [ "Jack Hotel", "Pop", "Nebraska", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 4, "Band" ],
+  [ "Twinsmith", "Rock", "Nebraska", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 5, "Band" ],
+  [ "AZP", "Folk", "Nebraska", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 6, "Band" ]
+]
+
+band_list.each do |name, genre, origin, website, bio, image, headline_order|
+  Band.create( name: name, genre: genre, origin: origin, website: website, bio: bio, image: image, headline_order: headline_order)
+end
+
+comedian_list = [
+  [ "Hank Misterson", "Nebraska", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 1],
+  [ "Larissa Mursterson", "Germany", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 2],
+  [ "Helen Masterson", "Illinois", "google.com", "bio bio bio bio bio bio bio bio", "/assets/boy.png", 3]
+]
+comedian_list.each do |name, origin, website, bio, image, headline_order|
+  Band.create( name: name, origin: origin, website: website, bio: bio, image: image, headline_order: headline_order)
+end
