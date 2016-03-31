8cfdcd5b534bcd4ddb98a5a46082cf74434190c0
Kyle Sandage <ksandage08@gmail.com>
2016-03-31 14:17:18 -0500

    trouble hidding days after one has been clicked

--- app/assets/javascripts/schedule_dropdown.js
+++ app/assets/javascripts/schedule_dropdown.js
@@ -2,8 +2,14 @@ var day_picker_box = document.getElementById("day_picker_box");
 var element_to_show = document.getElementsByClassName("day_picker__text");
 
 day_picker_box.addEventListener("click", function(){
-  alert("You Clicked Me!");
   for (var i = element_to_show.length - 1; i >= 0; i--) {
     element_to_show[i].classList.remove("day_picker__text--hide")
+    element_to_show[i].addEventListener("click", function(){
+      alert(event.target.innerHTML)
+      if (element_to_show[i] !== event.target){
+        element_to_show[i].classList.add("day_picker__text--hide")
+      }
+
+    })
   }
 });
