a3e322c554c0240230745cbe5b923fee5cc1953b
Kyle Sandage <ksandage08@gmail.com>
2016-03-31 18:38:13 -0500

    I'm the greatest programmer ever

--- app/assets/javascripts/schedule_dropdown.js
+++ app/assets/javascripts/schedule_dropdown.js
@@ -1,7 +1,26 @@
-// var day_picker_box = document.getElementById("day_picker_box");
-// var element_to_show = document.getElementsByClassName("day_picker__text");
-// var clicked = "";
+var day_picker_box = document.getElementById("day_picker_box");
+var thurs = document.getElementById("thurs");
+var fri = document.getElementById("fri");
+var sat = document.getElementById("sat");
+var other_days = document.getElementById("other_days");
+var current_day = document.getElementById("current_day");
 
+day_picker_box.addEventListener("click", function(){
+  other_days.style.display = "block";
+});
+
+thurs.addEventListener("click", function(){
+  current_day.innerHTML = event.target.innerHTML
+  other_days.style.display = "none";
+});
+fri.addEventListener("click", function(){
+  current_day.innerHTML = event.target.innerHTML
+  other_days.style.display = "none";
+});
+sat.addEventListener("click", function(){
+  current_day.innerHTML = event.target.innerHTML
+  other_days.style.display = "none";
+});
 
 // for (var i = element_to_show.length - 1; i >= 0; i--) {
 //    element_to_show[i].addEventListener("click", function(){
@@ -17,11 +36,7 @@
 //   });
 // }
    
-// day_picker_box.addEventListener("click", function(){
-//   for (var i = element_to_show.length - 1; i >= 0; i--) {
-//     element_to_show[i].classList.remove("day_picker__text--hide")
-//   };
-// });
+
 
 
      
