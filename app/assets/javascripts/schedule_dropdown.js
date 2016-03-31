5431029f2f562d15b4c0d3d2409b506f3cd54b77
Kyle Sandage <ksandage08@gmail.com>
2016-03-31 18:21:45 -0500

    different approach to day drop down

--- app/assets/javascripts/schedule_dropdown.js
+++ app/assets/javascripts/schedule_dropdown.js
@@ -1,15 +1,27 @@
-var day_picker_box = document.getElementById("day_picker_box");
-var element_to_show = document.getElementsByClassName("day_picker__text");
+// var day_picker_box = document.getElementById("day_picker_box");
+// var element_to_show = document.getElementsByClassName("day_picker__text");
+// var clicked = "";
 
-day_picker_box.addEventListener("click", function(){
-  for (var i = element_to_show.length - 1; i >= 0; i--) {
-    element_to_show[i].classList.remove("day_picker__text--hide")
-    element_to_show[i].addEventListener("click", function(){
-      alert(event.target.innerHTML)
-      if (element_to_show[i] !== event.target){
-        element_to_show[i].classList.add("day_picker__text--hide")
-      }
 
-    })
-  }
-});
+// for (var i = element_to_show.length - 1; i >= 0; i--) {
+//    element_to_show[i].addEventListener("click", function(){
+//     clicked = (event.target);
+//     alert(clicked.innerHTML);
+//     for (var i = element_to_show.length - 1; i >= 0; i--) {
+
+//       if (element_to_show[i] !== event.target){
+//         debugger;
+//         element_to_show[i].classList.add("day_picker__text--hide");
+//       }
+//     } 
+//   });
+// }
+   
+// day_picker_box.addEventListener("click", function(){
+//   for (var i = element_to_show.length - 1; i >= 0; i--) {
+//     element_to_show[i].classList.remove("day_picker__text--hide")
+//   };
+// });
+
+
+     
