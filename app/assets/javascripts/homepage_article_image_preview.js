c6d80db70f4b7324765301021012c2ea184578d2
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-31 22:09:39 -0500

    working towards changing pictures

---
+++ app/assets/javascripts/homepage_article_image_preview.js
@@ -0,0 +1,11 @@
+//if you hover over a headline, it turns orange 
+//the photo changes to the selected headline's image
+var homepage_headline = document.getElementsByClassName("home_news__headlines");
+var featured_headline = document.getElementsByClassName("article_feature");
+
+for (var i = homepage_headline.length - 1; i >= 0; i--) {
+  var headline = homepage_headline[i]
+  headline.addEventListener("mouseover",function(){
+    featured_headline.classList.remove("article_feature");
+})};
+
