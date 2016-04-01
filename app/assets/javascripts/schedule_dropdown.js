feb9c9aeca1e2f1b23975997aefca87b8b3263c9
Kyle Sandage <ksandage08@gmail.com>
2016-03-31 19:43:45 -0500

    Thursday and friday shows toggle

--- app/assets/javascripts/schedule_dropdown.js
+++ app/assets/javascripts/schedule_dropdown.js
@@ -4,6 +4,8 @@ var fri = document.getElementById("fri");
 var sat = document.getElementById("sat");
 var other_days = document.getElementById("other_days");
 var current_day = document.getElementById("current_day");
+var thursday_shows = document.getElementById("thursday_shows");
+var friday_shows = document.getElementById("friday_shows");
 
 day_picker_box.addEventListener("click", function(){
   other_days.style.display = "block";
@@ -12,29 +14,23 @@ day_picker_box.addEventListener("click", function(){
 thurs.addEventListener("click", function(){
   current_day.innerHTML = event.target.innerHTML
   other_days.style.display = "none";
+  thursday_shows.style.display = "block";
+  friday_shows.style.display = "none";
 });
+
 fri.addEventListener("click", function(){
   current_day.innerHTML = event.target.innerHTML
   other_days.style.display = "none";
+  thursday_shows.style.display = "none";
+  friday_shows.style.display = "block";
 });
+
 sat.addEventListener("click", function(){
   current_day.innerHTML = event.target.innerHTML
   other_days.style.display = "none";
 });
 
-// for (var i = element_to_show.length - 1; i >= 0; i--) {
-//    element_to_show[i].addEventListener("click", function(){
-//     clicked = (event.target);
-//     alert(clicked.innerHTML);
-//     for (var i = element_to_show.length - 1; i >= 0; i--) {
 
-//       if (element_to_show[i] !== event.target){
-//         debugger;
-//         element_to_show[i].classList.add("day_picker__text--hide");
-//       }
-//     } 
-//   });
-// }
    
 
 
