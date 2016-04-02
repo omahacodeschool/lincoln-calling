96e06f259f0e0b392829b5a81d59c6ee0d2915fc
Shannon Jackson <shannonfromomaha@gmail.com>
2016-04-02 09:55:17 -0500

    it's beautiful.

--- app/assets/javascripts/homepage_article_image_preview.js
+++ app/assets/javascripts/homepage_article_image_preview.js
@@ -2,11 +2,9 @@
 //the photo changes to the selected headline's image
 function hide_old_image(featuredclass){
   var featured_image = document.getElementsByClassName(featuredclass);
-
   for (var i = featured_image.length - 1; i >= 0; i--) {
     featured_image[i].classList.add("is_hidden");
   };
-
 };
 function remove_featured_class(featuredclass){
   var featured_headline = document.getElementsByClassName(featuredclass);
@@ -20,7 +18,6 @@ var homepage_headline = document.getElementsByClassName("home_news__headlines--h
 for (var i = homepage_headline.length - 1; i >= 0; i--) {
   var headline = homepage_headline[i];
   headline.addEventListener("mouseover",function(event){
-    console.log(event.target.id);
     var headline_image = document.getElementById("image_"+(event.target.id));
     hide_old_image("feature_image")
     remove_featured_class("home_news__headlines--selected")
