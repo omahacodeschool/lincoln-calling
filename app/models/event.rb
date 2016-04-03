2982fdec9d389cbd3f23e3a99a0bdfa7a65600c4
Shannon Jackson <shannonfromomaha@gmail.com>
2016-04-03 17:30:58 -0500

    getting weird errors i don't understand. going to jump into new new territory. if i break everything, this is the point i want to come back to

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,7 +11,7 @@ class Event < ActiveRecord::Base
   #returns the abbreviated month and day number
   #example: Oct. 6 
   def monthday
-    self.start_date_time.strftime("%b. %e")
+    self.start_date_time.strftime("%b.%e")
   end
   #returns event's start time
   #example: 1:30pm OR 12:45am etc
