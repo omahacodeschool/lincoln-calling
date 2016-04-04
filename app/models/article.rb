21b7b6f82cb52f773ee825d8e65792eee2b6cc14
Shannon Jackson <shannonfromomaha@gmail.com>
2016-04-03 19:55:37 -0500

    all tests pass except class methods and anything that deals with more than one Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 #Model for Article--has two main classes News and Visitor, and two additional, lesser used classes, About and FAQ
 #The schema for the model includes title:string user_id:integer(user id of the author), article_content:text, image:string(article image),publish_date_time:date_time, and type:string(where we establish a Class of News or Visitor, or less often, FAQ or About)
 class Article < ActiveRecord::Base 
+  #user_access returns the User object of the author of an article
   def user_access
     x = self.user_id
     y = User.find_by_id(x)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#user_full_name" do
+    it "returns first and last name of user" do
+      user = User.new(first_name: "John", last_name: "Doe")
+      assert_equal("John Doe", user.user_full_name)
+    end
+  end
 end
