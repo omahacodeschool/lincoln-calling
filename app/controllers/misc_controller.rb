d6f60b0190d16b4949acb6b53094d5a7d289b9cb
Beth Haubert <bethany.haubert@gmail.com>
2016-04-05 11:52:46 -0500

    tickets from db

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,6 +1,7 @@
 class MiscController < ApplicationController
   def home
     @events = Event.all
+    @tickets = Ticket.all
     @articles = Feature.all.order("id DESC").limit(4)
     @headlineartists= Band.order(:headline_order).limit(3)
     @otherartists = Band.where('headline_order != ?', 1).where('headline_order != ?', 2).where('headline_order != ?', 3)
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,10 @@
 #a Model to store information about Tickets, which HearNebraska sells through EventBrite.
 #schema includes name:string, link:string, price_in_cents:integer(we store the ticket prices in cents for maximum accuracy), eventbrite_id:integer, sold_out:boolean
 class Ticket < ActiveRecord::Base
+  def format_price
+    price = self.price_in_cents.to_s
+    shortened_price = price.slice(0..1)
+    shortened_price.insert(0, "$")
+    return shortened_price
+  end
 end
