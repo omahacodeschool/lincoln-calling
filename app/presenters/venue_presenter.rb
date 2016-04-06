6223b7106a29e2304f25d7aeadb543413199ffa1
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-06 17:42:48 -0500

    question

--- app/presenters/venue_presenter.rb
+++ app/presenters/venue_presenter.rb
@@ -23,13 +23,38 @@ class VenuePresenter
   end
 
   def shows_with_open_time_slots(shows_at_venue_this_day)
-    thursday_start_time = Time.parse("2016-10-06 17:00:00").to_s
-    thursday_start_time.sub! '-0500', 'UTC'
-    @shows_with_open_time_slots = []
+    @array_of_events_with_free_blocks = []
+    current_time_check = Time.parse("2016-10-06 17:00:00").to_s
+    current_time_check.sub! '-0500', 'UTC'
+    current_time_check = Time.parse(current_time_check)
+    puts "current_time_check is #{current_time_check}"
     i = 0
+    empty_block_count = 0
     start_of_show = shows_at_venue_this_day[i].start_date_time
-    puts "start of show is #{start_of_show}"
-    puts "PLUS FIFTEEN IS #{start_of_show_plus_fifteen = start_of_show + 15*60}"
+    puts "start_of_show is #{start_of_show}"
+
+    until current_time_check == start_of_show
+      empty_block_count += 1
+      puts "empty_block_count is #{empty_block_count}"
+      if empty_block_count == 4
+        puts "ADD AN EMPTY HOUR BLOCK..."
+        empty_block_count = 0
+      end
+      current_time_check = current_time_check + 15*60
+      puts "current_time_check is #{current_time_check}"
+    end
+
+    if empty_block_count == 3
+      puts "ADD 45 MINUTE BLOCK HERE"
+    elsif empty_block_count == 2
+      puts "ADD 30 MINUTE BLOCK HERE"
+    elsif empty_block_count == 1
+      puts "ADD 15 MINUTE BLOCK HERE"
+    end
+
+    puts "ADD SHOW BLOCK HERE"
+    i += 1
+    puts "i is #{i}"
   end
 
   # returns [#<Event:0x007fcf920a1f90
