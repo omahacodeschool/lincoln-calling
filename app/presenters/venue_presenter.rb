4892d9c13bc3667a65e00bc788dbad71f359c839
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-07 16:56:07 -0500

    everything is broken.

--- app/presenters/venue_presenter.rb
+++ app/presenters/venue_presenter.rb
@@ -8,9 +8,11 @@ class VenuePresenter
     return @venue
   end
 
+  #this is the method actually being called in the view.
   def shows_at_venue_per_day(week_day)
     shows_at_venue = Event.where("venue_id" => @venue.id)
     @shows_at_venue_per_day = []
+    @shows_with_open_time_slots = []
     shows_at_venue.each do |show|
       if show.weekday == week_day
         @shows_at_venue_per_day << show
@@ -19,53 +21,73 @@ class VenuePresenter
       end
     end
     @shows_at_venue_per_day.sort_by!{ |obj| obj.start_date_time}
-    start_time_and_first_show = shows_start_time(@shows_at_venue_per_day)
-    start_time_first_show = shows_start_time(@shows_at_venue_per_day)
+    shows_with_blanks
+    return @shows_with_blanks
   end
 
-  def shows_start_time(shows_at_venue_this_day)
-    @array_of_events_until_start = []
+  def shows_with_blanks
+    @shows_with_blanks = []
     current_time_check = Time.parse("2016-10-06 17:00:00").to_s
     current_time_check.sub! '-0500', 'UTC'
     current_time_check = Time.parse(current_time_check)
-    empty_block_count = 0
-    start_of_show = shows_at_venue_this_day[0].start_date_time
+    @i = 0
 
+    until @i == @shows_at_venue_per_day.length
+      puts "@shows_at_venue_per_day is #{@shows_at_venue_per_day} It's #{@shows_at_venue_per_day.length} elements in length."
+      start_of_show = @shows_at_venue_per_day[@i].start_date_time
+
+      array_of_events_until_show = find_show_start_time(current_time_check, start_of_show)
+      puts "array_of_events_until_show is #{array_of_events_until_show}"
+      blank_spaces_to_array(array_of_events_until_show)
+      puts "blanks added to array--@shows_with_blanks is #{@shows_with_blanks}"
+      @shows_with_blanks << @shows_at_venue_per_day[@i]
+      puts "show added to array--@shows_with_blanks is #{@shows_with_blanks}"
+
+      @i += 1
+      puts "@i is #{@i}"
+      current_time_check = end_time_of_previous_show
+      puts "current_time_check is #{current_time_check}"
+    end
+  end
+
+  def find_show_start_time(current_time_check, start_of_show)
+    array_of_events_until_show = []
+    empty_block_count = 0
     until current_time_check == start_of_show
       if empty_block_count == 4
-        @array_of_events_until_start = check_if_show_within_hour(current_time_check, start_of_show, @array_of_events_until_start)
+        array_of_events_until_show = check_if_show_within_hour(current_time_check, start_of_show, array_of_events_until_show)
         empty_block_count = 0
       end
       current_time_check = current_time_check + 15*60
       empty_block_count += 1
     end
-    return start_time_and_first_show(@array_of_events_until_start, shows_at_venue_this_day)
+    return array_of_events_until_show
   end
 
-  def start_time_and_first_show(array_of_events_until_start, shows_at_venue_this_day)
-    @shows_with_open_time_slots = []
-    array_of_events_until_start.each do |blank|
-      @shows_with_open_time_slots << blank
+  def blank_spaces_to_array(array_of_events_until_show)
+    array_of_events_until_show.each do |blank|
+      @shows_with_blanks << blank
     end
-    @shows_with_open_time_slots << shows_at_venue_this_day[0]
-    return @shows_with_open_time_slots
+    return @shows_with_blanks
   end
 
-  def check_if_show_within_hour(current_time_check, start_of_show, array_of_events_until_start)
+  def check_if_show_within_hour(current_time_check, start_of_show, array_of_events_until_show)
     if start_of_show == current_time_check + 45*60
-      array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 45*60)
+      array_of_events_until_show << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 45*60)
     elsif start_of_show == current_time_check + 30*60
-      array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 30*60)
+      array_of_events_until_show << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 30*60)
     elsif start_of_show == current_time_check + 15*60
-      array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 15*60)
+      array_of_events_until_show << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 15*60)
     else
-      array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 1.hour)
+      array_of_events_until_show << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 1.hour)
     end
-    return array_of_events_until_start
+    return array_of_events_until_show
   end
 
-  def end_time_of_last_show(shows_with_open_time_slots)
-    end_of_last_show = shows_with_open_time_slots.last.end_date_time
+  def end_time_of_previous_show
+    end_of_previous_show = @shows_with_blanks.last.end_date_time
+    puts "end_of_previous_show is #{end_of_previous_show}"
+    return end_of_previous_show
   end
 
   # returns [#<Event:0x007fcf920a1f90
