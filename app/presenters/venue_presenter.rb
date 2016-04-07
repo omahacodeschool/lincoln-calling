d80d2a8cc896fbd198617fa452de6cef0110e469
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-07 12:07:23 -0500

    Working to pick up time of first event even if there's a massive monster method that needs a shitload of refactoring

--- app/presenters/venue_presenter.rb
+++ app/presenters/venue_presenter.rb
@@ -18,44 +18,67 @@ class VenuePresenter
         next
       end
     end
-    @shows_at_venue_per_day = @shows_at_venue_per_day.sort_by{ |obj| obj.start_date_time}
-    self.shows_with_open_time_slots(@shows_at_venue_per_day)
+    @shows_at_venue_per_day.sort_by!{ |obj| obj.start_date_time}
+    start_time = shows_start_time(@shows_at_venue_per_day)
+    start_time_first_show = start_time_and_first_show(start_time, @shows_at_venue_per_day)
   end
 
-  def shows_with_open_time_slots(shows_at_venue_this_day)
-    @array_of_events_with_free_blocks = []
+  def shows_start_time(shows_at_venue_this_day)
+    @array_of_events_until_start = []
     current_time_check = Time.parse("2016-10-06 17:00:00").to_s
     current_time_check.sub! '-0500', 'UTC'
     current_time_check = Time.parse(current_time_check)
     puts "current_time_check is #{current_time_check}"
-    i = 0
     empty_block_count = 0
-    start_of_show = shows_at_venue_this_day[i].start_date_time
+    start_of_show = shows_at_venue_this_day[0].start_date_time
     puts "start_of_show is #{start_of_show}"
 
     until current_time_check == start_of_show
-      empty_block_count += 1
-      puts "empty_block_count is #{empty_block_count}"
       if empty_block_count == 4
-        puts "ADD AN EMPTY HOUR BLOCK..."
+        if start_of_show == current_time_check + 45*60
+          @array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 45*60)
+        elsif start_of_show == current_time_check + 30*60
+          @array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 30*60)
+        elsif start_of_show == current_time_check + 15*60
+          @array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 15*60)
+        else
+          @array_of_events_until_start << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 1.hour)
+        end
         empty_block_count = 0
       end
+
       current_time_check = current_time_check + 15*60
       puts "current_time_check is #{current_time_check}"
-    end
 
-    if empty_block_count == 3
-      puts "ADD 45 MINUTE BLOCK HERE"
-    elsif empty_block_count == 2
-      puts "ADD 30 MINUTE BLOCK HERE"
-    elsif empty_block_count == 1
-      puts "ADD 15 MINUTE BLOCK HERE"
+      empty_block_count += 1
+      puts "empty_block_count is #{empty_block_count}"
     end
 
     puts "ADD SHOW BLOCK HERE"
-    i += 1
-    puts "i is #{i}"
+    return @array_of_events_until_start
   end
+
+  def start_time_and_first_show(array_of_events_until_start, shows_at_venue_this_day)
+    @shows_with_open_time_slots = []
+    array_of_events_until_start.each do |blank|
+      @shows_with_open_time_slots << blank
+    end
+    @shows_with_open_time_slots << shows_at_venue_this_day[0]
+    return @shows_with_open_time_slots
+  end
+
+
+  # from Steven:
+  # until shows.empty?
+  #     if shows.first.start_date_time == current_time_check
+  #       @array << shows.shift
+  #     else
+  #       @array << Event.new(start_date_time: current_time_check, end_date_time: current_time_check + 1.hour)
+  #       @array<< shows.shift if shows.first.start_date_time == current_time_check
+  #     end
+  #     current_time_check = @array.last.end_date_time
+  #   end
+  #   @array
 
   # returns [#<Event:0x007fcf920a1f90
   # id: 1,
