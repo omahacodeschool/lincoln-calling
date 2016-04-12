35b6a43fa7cf087d93996c9f94c096d5bd6ff409
Meagan Jungman <meagan.jungman@gmail.com>
2016-04-11 23:28:03 -0500

    styling up on news page. lightbox worthing/functional. css classes only seem to size for dimensions up to tablet. will need to check in with design team

--- app/assets/javascripts/events_lightbox.js
+++ app/assets/javascripts/events_lightbox.js
@@ -5,7 +5,7 @@
     });
 
 
-if ( document.getElementsByClassName("bands__headliners").length !== 0){
+if(document.getElementsByClassName("bands__headliners").length !== 0 || document.getElementsByClassName("home_emails").length !== 0){
     $(".day_link").on('click', function(){
       var dayDisplay = $(this).attr('value');
       $( ".shows" ).hide();
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,13 @@
 class NewsController < ApplicationController 
   def index
     @articles = Feature.all.order("id DESC").limit(6)
+    @events = Event.all
+    th_shows = @events.byday("Thursday")
+    fri_shows = @events.byday("Friday")
+    sat_shows = @events.byday("Saturday")
+    @day1 = VenuePresenter.new(th_shows).events_with_venue_and_artist_info
+    @day2 = VenuePresenter.new(fri_shows).events_with_venue_and_artist_info
+    @day3 = VenuePresenter.new(sat_shows).events_with_venue_and_artist_info
   end
   def show
     @article = Feature.find_by_id(params[:id])
