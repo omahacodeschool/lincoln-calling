550dba3269d5b5be3408bd365732a51acc6626c5
Meagan Jungman <meagan.jungman@gmail.com>
2016-04-12 16:07:58 -0500

    added artist lightboxes functionality to schedule for melinda. pushing to master so that she can see it. Haven't fixed the sponsor page yet

--- app/assets/javascripts/artist_profile.js
+++ app/assets/javascripts/artist_profile.js
@@ -1,23 +1,20 @@
 // used in artist views: index, index_comedians, search
 
 // functionality for artist profile/searching pages/profiles
-if(document.getElementsByClassName("artistInfo").length !== 0){
   
   // checks the value of the selected comedian class. sets that to selectedArist variable.
   //finds the div with an ID identical to selectedArtist. Adds overlay class to div. Also makes it visible by removing visible class.
-  $('.artists').on('click', function(){
-    console.log(this);
-    var selectedArtist = $(this).attr('value');
-    $(`#${selectedArtist}`).removeClass('visible');
-    $(`#${selectedArtist}`).addClass('overlay');
-    var imageSize = $(`#artistPicture${selectedArtist}`).height();
-    console.log(imageSize);
-    $('.artist_profile__scroll_bar').height(imageSize);
-  });
-  // removes the artist profile overlay popup window and sets the information to a hidden state.
-  $('.close_button').on( "click", function(){
-    $('.artistInfo').removeClass( "overlay");
-     $('.artistInfo').addClass( "visible");
-  });
-
-};
+$('.artists').on('click', function(){
+  console.log(this);
+  var selectedArtist = $(this).attr('value');
+  $(`#${selectedArtist}`).removeClass('visible');
+  $(`#${selectedArtist}`).addClass('overlay');
+  var imageSize = $(`#artistPicture${selectedArtist}`).height();
+  console.log(imageSize);
+  $('.artist_profile__scroll_bar').height(imageSize);
+});
+// removes the artist profile overlay popup window and sets the information to a hidden state.
+$('.close_button').on( "click", function(){
+  $('.artistInfo').removeClass( "overlay");
+   $('.artistInfo').addClass( "visible");
+});
