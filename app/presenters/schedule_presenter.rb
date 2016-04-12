7fca8c8f7085335e87dda5b9aa56b9352e9c897a
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-12 17:27:30 -0500

    Added tomdocs

--- app/presenters/schedule_presenter.rb
+++ app/presenters/schedule_presenter.rb
@@ -4,6 +4,9 @@ class SchedulePresenter
     @venue = venue_object
   end
 
+  #sets date for weekday of festival passed in
+  #
+  #returns DateTime object
   def find_days_start_time(weekday)
     if weekday == "Thursday"
       festival_start = DateTime.new(2016,10,6,17,00,00, '+0')
@@ -15,7 +18,11 @@ class SchedulePresenter
     return festival_start
   end
 
-  #this is the method actually being called in the view.
+  #this is the method being called in the view.
+  #
+  #calls other methods to return ordered shows with empty time blocks between shows
+  #
+  #returns Array of Event objects
   def shows_at_venue_per_day(week_day)
     festival_start = find_days_start_time(week_day)
     shows_at_venue = Event.where("venue_id" => @venue.id)
@@ -33,6 +40,9 @@ class SchedulePresenter
     return @shows_with_blanks
   end
 
+  #calls other methods to add empty time slots between shows
+  #
+  #returns Array of Event objects
   def shows_with_blanks(festival_start)
     @shows_with_blanks = []
     @current_time_check = festival_start
@@ -48,6 +58,9 @@ class SchedulePresenter
     return @shows_with_blanks
   end
 
+  #creates empty time blocks to surround each show
+  #
+  #returns Array of Event objects
   def find_show_start_time(start_of_show)
     empty_block_count = 0
     check_if_show_within_hour(start_of_show)
@@ -64,6 +77,9 @@ class SchedulePresenter
     return @shows_with_blanks
   end
 
+  #checks forward at the hour to see if there's a show within the next hour
+  #
+  #returns Array of Event objects
   def check_if_show_within_hour(start_of_show)
     if start_of_show == @current_time_check + 45.minutes
       @shows_with_blanks << Event.new(start_date_time: @current_time_check, end_date_time: @current_time_check + 45.minutes)
@@ -72,13 +88,15 @@ class SchedulePresenter
     elsif start_of_show == @current_time_check + 15.minutes
       @shows_with_blanks << Event.new(start_date_time: @current_time_check, end_date_time: @current_time_check + 15.minutes)
     end
-
     if @shows_with_blanks.empty? == false
       @current_time_check = @shows_with_blanks.last.end_date_time
     end
     return @shows_with_blanks
   end
 
+  #gets end time of previous show in @shows_with_blanks Array
+  #
+  #returns DateTime object
   def end_time_of_previous_show
     if @shows_with_blanks.last != nil
       end_of_previous_show = @shows_with_blanks.last.end_date_time
@@ -86,6 +104,9 @@ class SchedulePresenter
     return end_of_previous_show
   end
 
+  #adds times after each show to fill in until next hour block
+  #
+  #returns Array of Event objects
   def check_previous_end_time(start_of_show, end_time_of_previous_show)
     if end_time_of_previous_show != nil || end_time_of_previous_show != start_of_show
       if end_time_of_previous_show.strftime("%M").include? "15"
@@ -110,6 +131,9 @@ class SchedulePresenter
     return @shows_with_blanks
   end
 
+  #creates blank objects to fill in until 2am end time each day
+  #
+  #returns Array of Event objects
   def fill_out_final_blanks(festival_start)
     @current_time_check = end_time_of_previous_show
     festival_end = festival_start + 9.hours
