9ccc92a0671944e4f13aa639703c8108b7ab53ba
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-13 18:51:44 -0500

    find_days_start_time test

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
-RSpec.describe Artist, type: :model do
+RSpec.describe SchedulePresenter, type: :model do
 
-
-
+  describe "#find_days_start_time" do
+    it "returns DateTime object for 5pm on day called" do
+    this_date = DateTime.new(2016,10,6,17,00,00, '+0')
+    venue_object = Venue.new
+    schedule_presenter = SchedulePresenter.new(venue_object)
+    assert_equal(this_date, schedule_presenter.find_days_start_time("Thursday"))
+    end
+  end
 
 end
