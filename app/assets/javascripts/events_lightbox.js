9d15514d9541328aaa5747550176b42b78f8c113
Meagan Jungman <meagan.jungman@gmail.com>
2016-04-13 12:15:26 -0500

    .

--- app/assets/javascripts/events_lightbox.js
+++ app/assets/javascripts/events_lightbox.js
@@ -5,11 +5,9 @@ $(".day_link").on('click', function(){
 });
 
 
-if(document.getElementsByClassName("bands__headliners").length !== 0 || document.getElementsByClassName("home_emails").length !== 0){
-    $(".day_link").on('click', function(){
-      var dayDisplay = $(this).attr('value');
-      $( ".shows" ).hide();
-      $( `.${dayDisplay}` ).show();
-      event.preventDefault();
-    });
-};
+$(".day_link").on('click', function(){
+  var dayDisplay = $(this).attr('value');
+  $( ".shows" ).hide();
+  $( `.${dayDisplay}` ).show();
+  event.preventDefault();
+});
