f2684098e9f1d664ab36bb0b52e6524e58e72331
Meagan Jungman <meagan.jungman@gmail.com>
2016-04-14 17:36:28 -0500

    working on artist index page

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   get "artists/list/comedians" => "artists#index_comedians"
   post "artists/search" => "artists#search"
   get "events/list" => 'events#index'
+  get "genres/:id" => 'genres#view'
   get "info/visitors" => 'visitors#index'
   get "info/visitors/:id" => 'visitors#show', as: :visitors_article
   get "info/faq" => 'info#faq'
