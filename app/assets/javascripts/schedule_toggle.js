335ce6dd07e1f8ea38ac12a6faccd8389da6fd3a
Melinda Deinert <melinda.deinert@gmail.com>
2016-04-18 16:18:24 -0500

    basic event listeners

--- app/assets/javascripts/schedule_toggle.js
+++ app/assets/javascripts/schedule_toggle.js
@@ -1,5 +1,22 @@
-if(document.getElementById("venue_shows_container") != null || document.getElementById("venue_shows_container") != undefined){
+if(document.getElementById("day_picker_box") != null || document.getElementById("day_picker_box") != undefined){
+  //scrolling left and right between venues on the schedule view
+  var left_tri_box = document.getElementById("left_tri_box");
+  var right_tri_box = document.getElementById("right_tri_box");
 
+
+
+
+  left_tri_box.addEventListener("click", function(){
+    console.log("LEFT CLICK DETECTED");
+  });
+
+
+
+
+
+  right_tri_box.addEventListener("click", function(){
+    console.log("RIGHT CLICK DETECTED");
+  });
 
 
 
