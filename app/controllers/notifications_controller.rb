cdfe1a8e8660fd5f1899e116c32526c53d7f4e16
Beth Haubert <bethany.haubert@gmail.com>
2016-04-30 18:36:53 -0500

    cleaned up lines from notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -17,32 +17,6 @@ class NotificationsController < ApplicationController
     redirect_to "/events/list"
   end
 
-
-
-#EXAMPLE FROM SMS-CRM
-  # Method takes a Touch object IF it is the most recent OUTGOING touch... and creates a text message from the data saved in the DB.
-  #returns nil.
-  #Also adds country code to phone number. Default is US ("+1")
-  # def send_sms
-  #   client = Client.find_by_id(params[:client_id])
-  #   touch = client.touches.create({message: params[:message_content], outgoing: true, read: true})
-
-  #   # Twilio credentials:
-  #   account_sid = ENV["TWILIO_ACCOUNT_ID"]
-  #   auth_token = ENV["TWILIO_AUTH_TOKEN"] 
-
-  #   # set up a client to talk to the Twilio REST API 
-  #   @twilio = Twilio::REST::Client.new(account_sid, auth_token) 
-     
-  #   # Sending an SMS:
-  #   @twilio.account.messages.create(
-  #     :from => client.business.business_phone, 
-  #     :to => client.phone_number, 
-  #     :body => touch.message
-  #   )
-  
-  # end
-
 end
 
 
