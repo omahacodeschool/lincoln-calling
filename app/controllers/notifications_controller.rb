7c284db6222a52df03c9d86c2367923f4d357b68
Beth Haubert <bethany.haubert@gmail.com>
2016-04-30 18:35:25 -0500

    twilio working. notifications functional

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -18,27 +18,6 @@ class NotificationsController < ApplicationController
   end
 
 
- def send_sms
-  account_sid = ENV["TWILIO_ACCOUNT_ID"]
-  auth_token = ENV["TWILIO_AUTH_TOKEN"] 
-  twilio_phone_number = "4027693024"
-
-  notification_list = Notification.all
-
-    notification_list.each do |notification|
-
-      number_to_send_to = notification.phone_number
-      message = event_message
-      @twilio_client = Twilio::REST::Client.new account_sid, auth_token 
-      @twilio_client.account.sms.messages.create(
-        :from => "+1#{twilio_phone_number}",
-        :to => number_to_send_to,
-        :body => "#{message}"
-      )
-    end
- end
-
-
 
 #EXAMPLE FROM SMS-CRM
   # Method takes a Touch object IF it is the most recent OUTGOING touch... and creates a text message from the data saved in the DB.
