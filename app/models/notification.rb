446e2629703a0ce42d38aac049ccabef2f0f7ce7
Beth Haubert <bethany.haubert@gmail.com>
2016-04-30 19:05:35 -0500

    added associations to event & notifications controller

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,5 @@
 class Notification < ActiveRecord::Base
+  belongs_to(:event)
 
   # This method defines the message that will get sent 
   # for each Notification object
@@ -10,24 +11,26 @@ class Notification < ActiveRecord::Base
     return "there is a concert soon"
   end
 
-  def Notification.send_sms
-  account_sid = ENV["TWILIO_ACCOUNT_ID"]
-  auth_token = ENV["TWILIO_AUTH_TOKEN"] 
-  twilio_phone_number = "4027693024"
+  def send_sms
+    account_sid = ENV["TWILIO_ACCOUNT_ID"]
+    auth_token = ENV["TWILIO_AUTH_TOKEN"] 
+    twilio_phone_number = "4027693024"
 
-  notification_list = Notification.all
+    @twilio_client = Twilio::REST::Client.new account_sid, auth_token 
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => self.phone_number,
+      :body => "#{self.event_message}"
+    )
+ end
 
-    notification_list.each do |notification|
-
-      number_to_send_to = notification.phone_number
-      message = notification.event_message
-      @twilio_client = Twilio::REST::Client.new account_sid, auth_token 
-      @twilio_client.account.sms.messages.create(
-        :from => "+1#{twilio_phone_number}",
-        :to => number_to_send_to,
-        :body => "#{message}"
-      )
+ def Notification.time_to_send_sms
+  events = Event.where(:start_date_time => Time.now + 1.hour)
+  events.each do |event|
+    event.notifications.each do |notification|
+      notification.send_sms
     end
+  end
  end
 
 end
