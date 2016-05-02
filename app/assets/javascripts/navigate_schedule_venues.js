405bbf6a633d6bc634a3b465763a69b39ad3b7b4
Sumeet Jain <sumeet@sumeetjain.com>
2016-05-02 08:14:07 -0500

    Navigate schedule using arrow-clicks

---
+++ app/assets/javascripts/navigate_schedule_venues.js
@@ -0,0 +1,33 @@
+// used in events/index.html.erb view
+
+$("#schedule-prev").click(function(e){
+  var $first_visible = $(".venue_box__text:visible:first");
+  var $first_hidden = $first_visible.prev(".venue_box__text");
+  var $last_visible = $(".venue_box__text:visible:last");
+
+  var $shows_to_hide = $(".venue_shows--hide-" + $last_visible.data("id"))
+  var $shows_to_reveal = $(".venue_shows--hide-" + $first_hidden.data("id"))
+  
+  if ($first_hidden.length > 0){
+    $last_visible.hide();
+    $first_hidden.show();
+    $shows_to_hide.hide();
+    $shows_to_reveal.show();
+  }
+});
+
+$("#schedule-next").click(function(e){
+  var $first_visible = $(".venue_box__text:visible:first");
+  var $last_visible = $(".venue_box__text:visible:last");
+  var $first_hidden = $last_visible.next(".venue_box__text");
+
+  var $shows_to_hide = $(".venue_shows--hide-" + $first_visible.data("id"))
+  var $shows_to_reveal = $(".venue_shows--hide-" + $first_hidden.data("id"))
+
+  if ($first_hidden.length > 0){
+    $first_visible.hide();
+    $first_hidden.show();
+    $shows_to_hide.hide();
+    $shows_to_reveal.show();
+  }
+});
