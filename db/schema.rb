e80ed35de43edde9a3446e422175d07b4a4a792a
Sumeet Jain <sumeet@sumeetjain.com>
2016-05-02 07:57:49 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160414210149) do
 
   # These are extensions that must be enabled in order to support this database
@@ -72,18 +71,18 @@ ActiveRecord::Schema.define(version: 20160414210149) do
     t.datetime "updated_at",      null: false
   end
 
-  create_table "notifications", force: :cascade do |t|
-    t.string   "phone_number"
-    t.integer  "event_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
-
   create_table "genres", force: :cascade do |t|
     t.string   "name"
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string   "phone_number"
+    t.integer  "event_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "plans", force: :cascade do |t|
