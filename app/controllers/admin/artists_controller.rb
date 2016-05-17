00c8be9e5d8b8d2dae5f9a7dd704814c53bc9241
Meagan Jungman <meagan.jungman@gmail.com>
2016-05-17 11:22:15 -0500

    fixed gmaps issue

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -13,29 +13,6 @@ module Admin
     #   Artist.find_by!(slug: param)
     # end
 
-    def artist_params
-      params.require(:artist).permit(:image_crop_x, :image_crop_y, :image_crop_w, :image_crop_h, ....)
-    end
-
-    def create
-      @artist = Artist.new(user_params)
-      respond_to do |format|
-        if @artist.save
-          format.html {
-            if params[:atist][:image].present?
-              render :crop  ## Render the view for cropping
-            else
-              redirect_to @artist, notice: 'Artist was successfully created.'
-            end
-          }
-          format.json { render action: 'show', status: :created, location: @artist }
-        else
-          format.html { render action: 'new' }
-          format.json { render json: @artist.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-
     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
     # for more information
   end
--- app/controllers/admin/bands_controller.rb
+++ app/controllers/admin/bands_controller.rb
@@ -15,28 +15,6 @@ module Admin
 
     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
     # for more information
-    def band_params
-      params.require(:band).permit(:image_crop_x, :image_crop_y, :image_crop_w, :image_crop_h)
-    end
-
-    def create
-      @band = Band.new(user_params)
-      respond_to do |format|
-        if @band.save
-          format.html {
-            if params[:band][:image].present?
-              render :crop  ## Render the view for cropping
-            else
-              redirect_to @band, notice: 'Band was successfully created.'
-            end
-          }
-          format.json { render action: 'show', status: :created, location: @band }
-        else
-          format.html { render action: 'new' }
-          format.json { render json: @band.errors, status: :unprocessable_entity }
-        end
-      end
-    end
 
   end
 end
