b46ae49942a460bc1b9cc605cdce8406de828afc
Michael Todd <mikejtodd@gmail.com>
2016-05-17 19:40:49 -0500

    Merging new Google Maps changes

--- app/assets/javascripts/homepage_article_image_preview.js
+++ app/assets/javascripts/homepage_article_image_preview.js
@@ -1,29 +1,9 @@
-//if you hover over a headline, it turns orange 
-//the photo changes to the selected headline's image
-function hide_old_image(featuredclass){
-  var featured_image = document.getElementsByClassName(featuredclass);
-  for (var i = featured_image.length - 1; i >= 0; i--) {
-    featured_image[i].classList.add("is_hidden");
-  };
-};
-function remove_featured_class(featuredclass){
-  var featured_headline = document.getElementsByClassName(featuredclass);
-    for (var i = featured_headline.length - 1; i >= 0; i--) {
-    featured_headline[i].classList.remove(featuredclass);
-  }
-}
-
-var homepage_headline = document.getElementsByClassName("home_news__headlines--headline");
-
-for (var i = homepage_headline.length - 1; i >= 0; i--) {
-  var headline = homepage_headline[i];
-  headline.addEventListener("mouseover",function(event){
-    var headline_image = document.getElementById("image_"+(event.target.id));
-    hide_old_image('feature_image')
-    remove_featured_class('home_news__headlines--selected')
-    headline_image.classList.remove('is_hidden');
-    headline_image.classList.add('feature_image')
-    event.target.classList.add('home_news__headlines--selected')    
-  })
-};
-
+$(document).ready(function(){
+    $('.headlines__text').hover(function(){
+        var index = $(this).data('index');
+        $('.images__news').removeClass('active');
+        $('.headlines__text').removeClass('active');
+        $(this).addClass('active');
+        $('.images__news').eq(index).addClass('active');
+    });
+});
