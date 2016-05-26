36e77b5479e82e707ba01024a4cf47275d5a916f
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-05-26 15:21:14 -0500

    removes for features

--- app/dashboards/feature_dashboard.rb
+++ app/dashboards/feature_dashboard.rb
@@ -8,7 +8,6 @@ class FeatureDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    id: Field::Number,
     title: Field::String,
     user: Field::BelongsTo,
     article_content: Field::Text,
@@ -25,7 +24,6 @@ class FeatureDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :id,
     :title,
     :user,
     :article_content,
@@ -35,7 +33,6 @@ class FeatureDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :id,
     :title,
     :user,
     :article_content,
