872f56986731e59f573ba4a634770b6d5faa068c
Michael Todd <mikejtodd@gmail.com>
2016-06-26 17:40:24 -0500

    Added auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    before_filter :authenticate
+    protected
+    def authenticate
+        authenticate_or_request_with_http_basic do |username, password|
+            username == ENV["SITE_LOGIN"] && password == ENV["SITE_PASSWORD"]
+        end
+    end
 end
