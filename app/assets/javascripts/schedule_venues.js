48630c5d4097498919607629beca7f7be59a47d3
Michael Todd <mikejtodd@gmail.com>
2016-06-27 20:08:21 -0500

    Schedule day change by ajax

--- app/assets/javascripts/schedule_venues.js
+++ app/assets/javascripts/schedule_venues.js
@@ -50,6 +50,42 @@ $(document).ready(function(){
     $('.days__day').click(function(){
         $('.days__select').addClass('select--show');
     });
+    
+    $('.select__day a').click(function(event){
+        $('.days__select').removeClass('select--show');
+        event.preventDefault();
+        var dayText = $(this).parent().data('date');
+        console.log(dayText);
+        $('.days__day.active').text(dayText);
+        $.get($(this).attr('href'), function(data) {
+            $('.schedule__concerts').replaceWith($(data).find('.schedule__concerts'));
+            if (Modernizr.mq('(min-width: 800px)')) {
+                venuesEnd = venuesStart + 5;
+            } else if (Modernizr.mq('(min-width: 600px)')) {
+                venuesEnd = venuesStart + 3;
+            } else {
+                venuesEnd = venuesStart + 1;
+            }
+            $('.schedule-nav__venues .venues__venue').removeClass('show');
+            $('.schedule-nav__venues .venues__venue').slice(venuesStart, venuesEnd).addClass('show');
+            $('.concerts__venue').removeClass('venue--show');
+            $('.concerts__venue').slice(venuesStart, venuesEnd).addClass('venue--show');
+            $('.concerts__venue').eq(venuesStart).addClass('first--venue');
+            $('.venue__concert').click(function(){
+                var id = $(this).data('id');
+                $.post('/artists/' + id)
+                .done(function(data){
+                    $('#artistName').text(data["name"]);
+                    $('#artistCity').text(data["origin"]);
+                    $('#artistWebsite').text(data["website"]);
+                    $('.info__bio').text(data["bio"]);
+                    $('.overlay__image').css('background-image', 'url(' + data["image"]["url"] + ')');
+                    $('body').css('overflow', 'hidden');
+                    $('.body__overlay').addClass('overlay--show');
+                });
+            });
+        });
+    });
 });
 
 $(window).resize(function(){
