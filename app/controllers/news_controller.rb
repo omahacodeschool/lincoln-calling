c95404c913f3f60a82b8e291661d5b5af0be9eb8
Michael Todd <mikejtodd@gmail.com>
2016-06-29 21:13:01 -0500

    Cleared footer of fake sponsors

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,17 +2,29 @@ class NewsController < ApplicationController
     def index
         @articles = Feature.all.order("created_at DESC").page params[:page]
         @events = Event.all
-        @day1 = VenuePresenter.new(@events.byday("Thursday")).events_with_venue_and_artist_info
-        @day2 = VenuePresenter.new(@events.byday("Friday")).events_with_venue_and_artist_info
-        @day3 = VenuePresenter.new(@events.byday("Saturday")).events_with_venue_and_artist_info
+        weekdays = []
+        @days = []
+        
+        @events.each do |event|
+            if weekdays.include?(event.start_date_time.wday) == false
+                @days.push(event.start_date_time)
+                weekdays.push(event.start_date_time.wday)
+            end
+        end
     end
   
     def show
         @article = Feature.friendly.find(params[:id])
         @events = Event.all
-        @day1 = VenuePresenter.new(@events.byday("Thursday")).events_with_venue_and_artist_info
-        @day2 = VenuePresenter.new(@events.byday("Friday")).events_with_venue_and_artist_info
-        @day3 = VenuePresenter.new(@events.byday("Saturday")).events_with_venue_and_artist_info
+        weekdays = []
+        @days = []
+        
+        @events.each do |event|
+            if weekdays.include?(event.start_date_time.wday) == false
+                @days.push(event.start_date_time)
+                weekdays.push(event.start_date_time.wday)
+            end
+        end
         @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     end
 end
