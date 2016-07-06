b621bdc69404f872946f316f7bd579bde4a39f05
Michael Todd <mikejtodd@gmail.com>
2016-07-05 20:34:39 -0500

    About page edits, hiding sections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     before_filter :authenticate
+    
+    def render_404
+        redirect_to :root
+    end
+    
     protected
+    
     def authenticate
         authenticate_or_request_with_http_basic do |username, password|
             username == ENV["SITE_LOGIN"] && password == ENV["SITE_PASSWORD"]
--- config/routes.rb
+++ config/routes.rb
@@ -18,25 +18,26 @@ Rails.application.routes.draw do
     
     devise_for :users
     root to: "misc#home"
-    get "/artists" => "artists#index"
-    post "/artists/search" => "artists#search"
-    post "/artists/:id" => "artists#show"
-    post "/artists/:id/prev" => "artists#prev"
-    post "/artists/:id/next" => "artists#next"
-    get "/comedy" => "artists#index_comedians"
-    post "/artists/search" => "artists#search"
-    get "/schedule" => 'events#index'
-    get "/schedule/:day" => 'events#index'
-    get "/artists/genres/:id" => 'genres#view'
-    get "/visitors" => 'visitor_guides#index'
-    get "/visitors/:id" => 'visitor_guides#show', as: :visitors_article
+    # get "/artists" => "artists#index"
+    # post "/artists/search" => "artists#search"
+    # post "/artists/:id" => "artists#show"
+    # post "/artists/:id/prev" => "artists#prev"
+    # post "/artists/:id/next" => "artists#next"
+    # get "/comedy" => "artists#index_comedians"
+    # post "/artists/search" => "artists#search"
+    # get "/schedule" => 'events#index'
+    # get "/schedule/:day" => 'events#index'
+    # get "/artists/genres/:id" => 'genres#view'
+    # get "/visitors" => 'visitor_guides#index'
+    # get "/visitors/:id" => 'visitor_guides#show', as: :visitors_article
     get "/faq" => 'info#faq'
     get "/about" => 'info#about'
-    get "news" => 'news#index'
-    get "news/:id" => "news#show", as: :news_article
-    get "sidebar/events" => 'sidebars#events'
-    get "sidebar/artists" => "sidebars#artists"
+    # get "news" => 'news#index'
+    # get "news/:id" => "news#show", as: :news_article
+    # get "sidebar/events" => 'sidebars#events'
+    # get "sidebar/artists" => "sidebars#artists"
     get "sponsors" => "sponsors#index"
     get "venues" => "venues#index"
-    post "notifications/plan_my_festival" => "notifications#plan_my_festival"
+    # post "notifications/plan_my_festival" => "notifications#plan_my_festival"
+    get '*unmatched_route', to: 'application#render_404'
 end
