be957661907a80d35b637301a0f8c59902c3d917
Michael Todd <mikejtodd@gmail.com>
2016-07-15 06:06:27 -0500

    Artist overlays

--- app/assets/javascripts/artist_profile.js
+++ app/assets/javascripts/artist_profile.js
@@ -9,15 +9,16 @@ function showArtist(id) {
         $('#artistName').text(artist["name"]);
         $('#artistCity').text(artist["origin"]);
         $('#artistWebsite').html('<a target="_blank" href="http://' + artist["website"] + '">' + artist["website"] + '</a>');
+        console.log(data["artist"]);
         $('.info__bio').text(data["artist"]["bio"]);
         $('.overlay__image').css('background-image', 'url(' + artist["image"]["url"] + ')');
         $('body').css('overflow', 'hidden');
         $('.body__overlay').addClass('overlay--show');
         overlayArtist = artist["id"];
-        $('#artistConcerts').html('');
-        for (var i = 0, l = concerts.length; i < l; i++) {
-            $('#artistConcerts').append('<div class="concerts__concert"><div class="concert__date">' + concerts[i]["start_time"] + '</div><div class="concert__venue">' + concerts[i]["venue"] + '</div></div>')
-        }
+        // $('#artistConcerts').html('');
+        // for (var i = 0, l = concerts.length; i < l; i++) {
+        //     $('#artistConcerts').append('<div class="concerts__concert"><div class="concert__date">' + concerts[i]["start_time"] + '</div><div class="concert__venue">' + concerts[i]["venue"] + '</div></div>')
+        // }
     });
 }
 
@@ -27,8 +28,10 @@ $(document).ready(function(){
     $('.artists__artist').click(showArtist);
     $('.shows__show p').click(showArtist);
     $('.venue__concert').click(showArtist);
+    $('.hero__headliners a').click(function(e){
+        e.preventDefault;
+    });
     $('.hero__headliners a').click(showArtist);
-    $('.hero__supporting a').click(showArtist);
     $('.overlay__close').click(function(){
         $('body').css('overflow', 'auto');
         $('.body__overlay').removeClass('overlay--show');
